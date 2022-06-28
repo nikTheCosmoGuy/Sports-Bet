629d761c2dcc5a4cf68e0a954c76f38973cfa3e0
Nikolas Iwanus <nikTheCosmoGuy@users.noreply.github.com>
2022-06-28 16:30:59 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 import os
 
+project_name = None
 
 def manage_gcp_auth(cert_path=cert_path):
     """Make connection to gcp authenticated"""
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = str(cert_path)
+
+bq_uri = None if project_name is None else f'bigquery://{project_name}'
