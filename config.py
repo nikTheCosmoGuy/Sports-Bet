f5a440dcdfe5e8f8271d72759c7f61651306c746
Nikolas Iwanus <nikTheCosmoGuy@users.noreply.github.com>
2022-06-28 15:11:28 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 from pathlib import Path
+import os
 
-cert_path = Path(r"C:") / "root" / "to" / "gcp_creds.json"
+
+def manage_gcp_auth(cert_path=cert_path):
+    """Make connection to gcp authenticated"""
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = str(cert_path)
